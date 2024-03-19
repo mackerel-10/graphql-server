8330c51691e1847415e46716f4585087b1d7d110
Sujin Jeon <sujinjeon@Sujinui-noteubug.local>
2024-03-19 15:49:01 +0900

    Feat: GraphQL type을 사용해 schema 정의

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
+import {schema} from './schema'
+import cors from 'cors';
 
 const app = express();
 
-// app.all('/graphql', createHandler({ schema }));
+// app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+app.all('/graphql', createHandler({ schema }));
 
 app.listen(8000, () => {
     console.log('listen...');
+    console.log('hi');
 });
--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,37 @@
-import { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+import { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, buildSchema } from "graphql";
 
+/*
+** 직접 GraphQL의 type schema를 생성
+*/
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }`
+);
+
+// Resolver
+// Provides a resolver function for each API endpoint
+const rootValue = {
+    hello: () => {
+        return "Hello world!";
+    }
+};
+
+(async () => {
+    const response = await graphql({
+        schema, // GraphQL Type
+        source: '{hello}', // Client Query
+        rootValue // Resolver
+    });
+    console.log(response);
+})();
+
+
+/*
+** 2
+*/
+/*
 // Construct GraphQL schema
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -17,6 +49,10 @@ const schema = new GraphQLSchema({
 
 const source = '{hello}'; // Query From Client
 
-graphql({schema, source}).then((result) => {console.log(result)});
+// schema와 같은 필드의 source가 들어왔을때 
+(async () => {
+    const result = await graphql({ schema, source });
+    console.log(result)
+})(); */
 
 export {schema};
